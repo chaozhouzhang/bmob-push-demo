193157627be32cb1b4f7a3014243d334ab8c8d5e
chaozhouzhang <chaozhouzhang@qq.com>
2017-08-25 16:46:03 +0800

    Add Notification

--- Push/app/src/main/java/cn/bmob/push/receiver/PushMessageReceiver.java
+++ Push/app/src/main/java/cn/bmob/push/receiver/PushMessageReceiver.java
@@ -8,6 +8,8 @@ import android.widget.Toast;
 import com.orhanobut.logger.Logger;
 
 import cn.bmob.push.PushConstants;
+import cn.bmob.push.R;
+import cn.bmob.push.util.Notify;
 
 /**
  * Created on 17/8/24 14:20
@@ -22,6 +24,15 @@ public class PushMessageReceiver extends BroadcastReceiver {
         if (intent.getAction().equals(PushConstants.ACTION_MESSAGE)) {
             Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
             Logger.i("客户端收到推送消息：" + msg);
+            Notify notify = new Notify(context);
+            notify.setId(msg.hashCode());
+            notify.setTitle(msg);
+            notify.setAutoCancel(true);
+            notify.setSmallIcon(R.mipmap.ic_launcher);
+            notify.notification();
+            notify.show();
         }
+
+
     }
 }
