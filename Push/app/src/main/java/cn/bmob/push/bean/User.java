02161c2cd8cfdcf8c6b26469d4253c661bc6dbb9
chaozhouzhang <chaozhouzhang@qq.com>
2018-06-11 08:45:48 +0800

    Update Push SDK v1.0.2

--- Push/app/src/main/java/cn/bmob/push/bean/User.java
+++ Push/app/src/main/java/cn/bmob/push/bean/User.java
@@ -8,13 +8,4 @@ import cn.bmob.v3.BmobUser;
 
 public class User extends BmobUser {
 
-    String nickname;
-
-    public String getNickname() {
-        return nickname;
-    }
-
-    public void setNickname(String nickname) {
-        this.nickname = nickname;
-    }
 }
